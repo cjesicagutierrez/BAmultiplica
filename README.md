ea09fe4e66d85ff090bd4178d75fe85e9726adb4
Jesica Gutierrez <gutijesi89@gmail.com>
2023-11-16 22:36:10 -0300

    cambio para trabajo final

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # BAmultiplica
+bienvenidos
+de que se trata en que me inspire
+que bibliotecas use
--- js/main.js
+++ js/main.js
@@ -1,2 +1,41 @@
 // comentario de linea
 /* comentario de bloque */
+
+/*BOTON DE CONTACTO*/
+
+const btnAlert = document.getElementById('btn-alert')
+btnAlert.addEventListener('click', () => {
+    Swal.fire({
+        icon: "success",
+        title: "Tu mensaje se envió con exito!",
+        showConfirmButton: false,
+        timer: 1500
+    });
+});
+
+/* ADIVINANZA*/
+
+let numero = Math.floor(Math.random() * 100+1);
+let intentos = 1;
+console.log(numero);
+
+function adivinar(){
+    let respuesta = document.getElementById("respuesta").value;
+    let resultado = document.getElementById("resultado");
+
+    if (respuesta == numero){
+        resultado.textContent = '¡Felicitaciones, adivinaste el número en ${intentos} intentos!';
+        resultado.style.color = "green";
+    } else if (respuesta > numero){
+        resultado.textContent = 'El número es menor, intenta de nuevo.';
+        resultado.style.color = "red";
+        intentos++;
+    } else {
+        resultado.textContent = 'El número es mayor, intenta de nuevo.';
+        resultado.style.color = "red";
+        intentos++;
+    }
+
+    document.getElementById("respuesta").value = "";
+}
+
